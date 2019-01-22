9f16ee54f102004de504cf2830594101cf0418a8
Abhinav Singh <adityasaha890@gmail.com>
2019-01-22 12:42:33 +0530

    G

--- oauth-google/db.js
+++ oauth-google/db.js
@@ -5,6 +5,10 @@ mongoose.connect(db_url).catch(err => console.log(err));
 
 const google_schema=new mongoose.Schema({name:String, email:String })
 const google_model=mongoose.model("google_model",google_schema);
+
+const meeting_schema=new mongoose.Schema({email:String,date:String,time:String});
+const meeting_model=mongoose.model("meeting",meeting_schema);
 module.exports = {
-    database_google:google_model
+    database_google:google_model,
+    meeting_model
 }
