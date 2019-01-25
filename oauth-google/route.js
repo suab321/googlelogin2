5121e70f36c7fec6a9ccb1a744ee007650aa63c7
Abhinav Singh <adityasaha890@gmail.com>
2019-01-25 16:12:14 +0530

    fd

--- oauth-google/route.js
+++ oauth-google/route.js
@@ -11,7 +11,7 @@ router.get('/login',
 )
 router.get('/google_redirect',passport.authenticate('google',{failureRedirect:'http://localhost:4200/login/student'}),(req,res)=>{
     console.log("my session"+req.user);
-    res.redirect(`http://localhost:4200/googlelogin/${req.user.email}/${req.user.name}/${req.user._id}`);
+    res.redirect(`https://fadmits.herokuapp.com/googlelogin/${req.user.email}/${req.user.name}/${req.user._id}`);
 })
 
 module.exports={router};
